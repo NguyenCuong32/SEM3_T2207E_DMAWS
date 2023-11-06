fc2222d5e392cca07bcadfc8d7a973423111867f
NgoMinhKhoi2603 <109575681+NgoMinhKhoi2603@users.noreply.github.com>
2023-11-06 20:22:03 +0700

    Khoi
    up 1

---
+++ AzureAPIDemo3/AzureAPIDemo3/DAL/UserDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AzureAPIDemo3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureAPIDemo3.DAL
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+        }
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserModel>().ToTable("User");
+        }*/
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
---
+++ AzureAPIDemo3/AzureAPIDemo3/Migrations/20231025140559_Mg3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AzureAPIDemo3.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mg3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
