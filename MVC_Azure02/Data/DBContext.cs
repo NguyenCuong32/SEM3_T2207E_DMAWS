5b3351dc30a0b7849135bb3960d4c667a6686536
dxthanh2002 <109643053+dxthanh2002@users.noreply.github.com>
2023-11-10 19:07:52 +0700

    3 push

---
+++ MVC_Azure02/Data/DBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Azure02.Models;
+
+namespace MVC_Azure02.Data
+{
+    public class DBContext: DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>().ToTable("Student");
+
+        }
+        DbSet<Student> Students;
+        public DbSet<MVC_Azure02.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ MVC_Azure02/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Azure02.Models
+{
+    public class Student
+    {
+        [Key]
+        public int IdStudent { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+
+
+    }
+}
